a06d02a5469dd03e0632aa5a1ec3ce6217ed6030
Anton Melnikov <happyhehog@gmail.com>
2021-03-15 12:06:44 +1000

    Add zsh-completions plugin

--- install.sh
+++ install.sh
@@ -18,6 +18,7 @@ function zsh() {
   sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
   git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
   git clone https://github.com/lukechilds/zsh-nvm ~/.oh-my-zsh/custom/plugins/zsh-nvm
+  git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
 }
 
 function neovim() {
